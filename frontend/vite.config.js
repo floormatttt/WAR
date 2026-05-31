235c9474d4316f9cc8e3341e814fe634e5f1cada
floormatttt <mli.mgmp@gmail.com>
2026-05-31 18:51:05 -0400

    Pages fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/WAR/',
+  // Defaults to '/' for custom domains or local dev, and '/WAR/' for the default github.io subfolder
+  base: process.env.GITHUB_PAGES_CUSTOM_DOMAIN ? '/' : (command === 'serve' ? '/' : '/WAR/'),
 }))
